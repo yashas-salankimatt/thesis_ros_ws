f0df847e4cb33f446e8508859ec1e28bc9a25825
Yahas Salankimatt <yashas.salankimatt@gmail.com>
2024-04-19 18:50:23 -0500

    Fixed ip address for docker container

--- ROS-TCP-Endpoint/launch/endpoint.py
+++ ROS-TCP-Endpoint/launch/endpoint.py
@@ -9,7 +9,7 @@ def generate_launch_description():
                 package="ros_tcp_endpoint",
                 executable="default_server_endpoint",
                 emulate_tty=True,
-                parameters=[{"ROS_IP": "10.211.55.6"}, {"ROS_TCP_PORT": 10000}],
+                parameters=[{"ROS_IP": "0.0.0.0"}, {"ROS_TCP_PORT": 10000}],
             )
         ]
     )
