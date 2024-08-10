099f287cfe72e01a701f0eb2cb62b7f6ee8d6039
Yashas Salankimatt <yashas.salankimatt@gmail.com>
2024-08-10 00:33:41 -0700

    Added init moveit code

--- dimensional/dimensional/grasp_pipeline_test.py
+++ dimensional/dimensional/grasp_pipeline_test.py
@@ -18,6 +18,9 @@ from moveit_msgs.msg import MoveItErrorCodes
 from moveit_configs_utils import MoveItConfigsBuilder, MoveItConfigs
 from tf2_ros import TransformListener, Buffer
 from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy
+from moveit.core.robot_state import RobotState
+from moveit.planning import MoveItPy, MultiPipelinePlanRequestParameters
+import time
 
 def show_mask(mask, ax, random_color=False, borders = True):
     if random_color:
@@ -62,6 +65,38 @@ def show_masks(image, masks, scores, point_coords=None, box_coords=None, input_l
         plt.axis('off')
         plt.show()
 
+def plan_and_execute(
+    robot,
+    planning_component,
+    logger,
+    single_plan_parameters=None,
+    multi_plan_parameters=None,
+    sleep_time=0.0,
+):
+    """Helper function to plan and execute a motion."""
+    # plan to goal
+    logger.info("Planning trajectory")
+    if multi_plan_parameters is not None:
+        plan_result = planning_component.plan(
+            multi_plan_parameters=multi_plan_parameters
+        )
+    elif single_plan_parameters is not None:
+        plan_result = planning_component.plan(
+            single_plan_parameters=single_plan_parameters
+        )
+    else:
+        plan_result = planning_component.plan()
+
+    # execute the plan
+    if plan_result:
+        logger.info("Executing plan")
+        robot_trajectory = plan_result.trajectory
+        robot.execute(robot_trajectory, controllers=[])
+    else:
+        logger.error("Planning failed")
+
+    time.sleep(sleep_time)
+
 class GraspPipelineTest(Node):
     def __init__(self):
         super().__init__('grasp_pipeline_test')
@@ -92,7 +127,24 @@ class GraspPipelineTest(Node):
         self.target_pose_publisher = self.create_publisher(PoseStamped, '/target_pose', 10)
         
         # Set up MoveIt2
-        # self.move_group = MoveGroupCommander("arm")  # Replace "arm" with your robot's arm group name
+        self.plan_env = MoveItPy(node_name="moveit_py")
+        self.arm = self.plan_env.get_planning_component("xarm6")
+
+        self.arm.set_start_state_to_current_state()
+
+        pose_goal = PoseStamped()
+        pose_goal.header.frame_id = "world"
+        pose_goal.pose.position.x = 0.5
+        pose_goal.pose.position.y = 0.0
+        pose_goal.pose.position.z = 0.5
+        pose_goal.pose.orientation.x = 0.0
+        pose_goal.pose.orientation.y = 0.0
+        pose_goal.pose.orientation.z = 0.0
+        pose_goal.pose.orientation.w = 1.0
+        self.arm.set_goal_state(pose_stamped_msg=pose_goal, pose_link="link6")
+
+        plan_and_execute(self.plan_env, self.arm, self.get_logger(), sleep_time=3.0)
+
         
         # Set up TF listener
         self.tf_buffer = Buffer()
