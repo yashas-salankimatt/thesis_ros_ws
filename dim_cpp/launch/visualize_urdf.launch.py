2b41301c19747cafa8ab7e32b9fdb54387919359
Yashas Salankimatt <yashas.salankimatt@gmail.com>
2024-09-30 22:38:08 -0700

    Made URDF for Alfred base with pan-tilt head

---
+++ dim_cpp/launch/visualize_urdf.launch.py
@@ -0,0 +1,63 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import Command, LaunchConfiguration
+from launch_ros.actions import LoadComposableNodes, Node
+from launch_ros.descriptions import ComposableNode
+
+def generate_launch_description():
+    # Get the path to the xacro file
+    xacro_file = os.path.join(
+        get_package_share_directory('dim_cpp'),
+        'urdf',
+        'alfred_base_descr.urdf.xacro'
+    )
+
+    # Process the xacro file to generate the robot description
+    robot_description = {'robot_description': Command(['xacro ', xacro_file])}
+
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        output='screen'
+    )
+
+    # Define the joint state publisher GUI node
+    joint_state_publisher_gui_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui',
+        output='screen'
+    )
+
+    # Define the robot state publisher node
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[robot_description]
+    )
+
+    # Define the RViz node
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', os.path.join(get_package_share_directory('dim_cpp'), 'rviz', 'visualize.rviz')]
+    )
+
+    # Create the launch description and populate
+    ld = LaunchDescription()
+
+    # Add the nodes to the launch description
+    ld.add_action(joint_state_publisher_node)
+    ld.add_action(joint_state_publisher_gui_node)
+    ld.add_action(robot_state_publisher_node)
+    ld.add_action(rviz_node)
+
+    return ld
